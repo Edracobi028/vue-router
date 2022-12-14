5cd18c1ab78d7a9e6a22925856e24766ef9b0019
Eduardo Razo <desarrollo@gatewayfabrication.com.mx>
2022-12-14 09:30:04 -0600

    Navegación programática 6/17

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,17 @@
       return{
         username: '',
       }
+    },
+    methods:{
+      iniciar(){
+        //Redirigir utilizando el objeto $router y push para Agregar un nuevo elemento al historial de navegación
+        this.$router.push('/chats');
+
+       /* this.$router.go(-1); //Redirije un paso atras */ 
+       /*  this.$router.replace('/chats') //La navegación no genera puntos en la historia y remplaza la existente */
+
+
+      }
     }
   }
 </script>
@@ -12,5 +23,6 @@
   <main>
     <h3>Introduce tu nombre de tu usuario </h3>
     <input type="text" placeholder="username" v-model="username">
+    <button @click="iniciar">Iniciar sesión</button>
   </main>
 </template>
